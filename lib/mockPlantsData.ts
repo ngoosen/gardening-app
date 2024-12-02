b942d351d13747d86075bb6ecd3b4b689190ddf6
Noémie Goosen <noemie.goosen@gmail.com>
2024-12-02 09:29:25 +0100

    feat: add mock plants data

---
+++ lib/mockPlantsData.ts
@@ -0,0 +1,42 @@
+export const mockPlantsData = {
+  content: [
+    {
+      id: 1,
+      version: 1,
+      name: "Monstera deliciosa",
+      description: "Exotic palm plant",
+      createdAt: "2023-10-01 15:45:15",
+      updatedAt: "2023-10-14 09:32:57"
+    },
+    {
+      id: 2,
+      version: 1,
+      name: "Monstera adansonii",
+      description: "Small exotic palm plant",
+      createdAt: "2023-10-01 15:45:15",
+      updatedAt: "2023-10-14 09:32:57"
+    },
+    {
+      id: 3,
+      version: 1,
+      name: "Calathea rufibarba",
+      description: "Fluffy leaves plant",
+      createdAt: "2023-10-01 15:45:15",
+      updatedAt: "2023-10-14 09:32:57"
+    },
+    {
+      id: 4,
+      version: 1,
+      name: "Calathea zebrina",
+      description: "Fat leaves plant",
+      createdAt: "2023-10-01 15:45:15",
+      updatedAt: "2023-10-14 09:32:57"
+    }
+  ],
+  page: {
+    size: 1,
+    number: 1,
+    totalElements: 4,
+    totalPages: 1
+  }
+};
